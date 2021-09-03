6b78e084a1a004d557aaf98f55903e21e654c8bc
mariavaghani <maria.vaghani@gmail.com>
2021-09-03 15:37:11 -0400

    fixed space complexity for largest_contiguous_subsum method

---
+++ anagrams.rb
@@ -0,0 +1,12 @@
+# PHASE 1.
+
+
+def anagram?
+    
+end
+
+
+
+
+p anagram?("elvis", "lives")    #=> true
+p anagram?("gizmo", "sally")    #=> false
--- big_o.rb
+++ big_o.rb
@@ -1,19 +1,4 @@
 
-# def my_min(arr)
-# 	min_found = false
-	
-# 	until min_found
-# 		min_found = true
-# 		arr.each_with_index do |ele1, idx1|
-# 			if min_ele > ele1
-# 				min_ele = ele1
-# 				min_found = false
-# 				p min_ele
-# 			end
-# 		end
-# 	end
-# 	min_ele
-# end
 
 def my_min(arr)
     min_num = arr.first
@@ -28,18 +13,20 @@ def my_min(arr)
 end
 
 
+
+
 # PHASE 2 O(n)
 
-# def my_min(arr)
-# 	min_ele = arr.first
-# 	arr.each_with_index do |ele1, idx1|
-# 		if ele1 < min_ele
-# 			min_ele = ele1
+def my_min(arr)
+	min_ele = arr.first
+	arr.each_with_index do |ele1, idx1|
+		if ele1 < min_ele
+			min_ele = ele1
 		
-# 		end
-# 	end
-# 	min_ele
-# end
+		end
+	end
+	min_ele
+end
 
 
 # list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
@@ -47,43 +34,43 @@ end
 
 # PHASE 1
 
-# def largest_contiguous_subsum(list)
-#     subarr = []
-#     (0...list.length).each do |idx1|
-#         (idx1...list.length).each do |idx2|
-#             subarr << list[idx1..idx2].sum
-#         end
-#     end
-#     subarr.max
-# end
+def largest_contiguous_subsum(list)
+    subarr = []
+    (0...list.length).each do |idx1| # n times
+        (idx1...list.length).each do |idx2|# n times 
+            subarr << list[idx1..idx2].sum# n^2(2*n)
+        end
+    end
+    subarr.max # 2n^3 +n => n^3
+end
 
 # PHASE 2
 
-# def largest_contiguous_subsum(list)
-#     largest_sum = list.first
-#     (0...list.length - 1).each do |idx|
-#         current_sum = [list[0..idx].sum, list[idx..-1].sum, list[idx], list[idx..-idx].sum, list[-1]].max
-#         largest_sum = [current_sum, largest_sum].max
-#     end
 
-#     largest_sum
-# end
 
 def largest_contiguous_subsum(list)
-    largest_sum = list.sum
-    
-    while list.length > 1
-        if list[0] > list[-1]
-            list.pop
-        else
-            list.shift
-        end
-        current_sum = list.sum
-        largest_sum = current_sum if current_sum > largest_sum
-    end
+	largest_sum = list.sum
+	current_sum = list.sum
+	while list.length > 1
+		if list[0] > list[-1]
+			current_sum = current_sum - list.pop
+		else
+			current_sum = current_sum - list.shift
+		end
+		largest_sum = current_sum if current_sum > largest_sum
+	end
 
-    largest_sum
+	largest_sum
 end
 
-# list = [5, 3, -7]
-# largest_contiguous_subsum(list) # => 8
+list = [5, 3, -7]
+p largest_contiguous_subsum(list) # => 8
+
+list = [2, 3, -6, 7, -6, 7]
+p largest_contiguous_subsum(list) # => 8 (from [7, -6, 7])
+
+list = [-5, -1, -3]
+p largest_contiguous_subsum(list) # => -1 (from [-1])
+
+list = [-2, 10, -6, 12, -6, 1]
+p largest_contiguous_subsum(list) # => 8 (from [7, -6, 7])
